25f36c8cd81a2fbe10e29b579e89c209e81c5b00
Oscar <link57795@gmail.com>
2019-03-14 10:41:07 -0600

    indica si las teclas capslock y bloqnum estan activas
    se borraron algunas libreria que sno se usan

--- KeyboardIndicator/src/Indicator.java
+++ KeyboardIndicator/src/Indicator.java
@@ -2,17 +2,11 @@
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
